5b66c114b4b7332b6e5da1387397f79a0ee5b226
melninie <twybow91@gmail.com>
2018-10-04 12:01:34 +0200

    Create models

--- models/m_message.php
+++ models/m_message.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mel
- * Date: 03/10/2018
- * Time: 15:49
- */
--- models/m_user.php
+++ models/m_user.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mel
- * Date: 03/10/2018
- * Time: 15:40
+
+include '../controllers/c_db_connect.php';
+$bdd = connect();
+
+/*
+ * Check that login is unique in DB
+ * Return boolean
  */
+function loginIsUnique($login)
+{
+    global $bdd;
+    $req = $bdd->prepare("SELECT * FROM user WHERE login = ?");
+    $req->execute(array($login));
+    $req->closeCursor();
+    return ($req->rowCount() == 0);
+}
+
+/*
+ * Add a new user in DB
+ * Return id of added user
+ */
+function addUser($login)
+{
+    global $bdd;
+    $req = $bdd->prepare("INSERT INTO user (login) VALUES (?)");
+    $req->execute(array($login));
+    $req->closeCursor();
+    return $bdd->lastInsertId();
+}
